395543d66df904b5ddd9f3e6ad2526d6ba7c5828
admirsaheta <81534875+admirsaheta@users.noreply.github.com>
2022-09-12 13:01:56 +0200

    presentation-identifier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+
+use api::language::{
+    get_language,
+};
+
+
+use actix_web::{HttpServer,App,web::Data, middleware::Logger};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+
+std::env::set_var("RUST_LOG", "debug");
+std::env::set_var("RUST_BACKTRACE", 1);
+env_logger::init();
+
+HttpServer::new(move || {
+    App::new()
+    .wrap(logger)
+    .service(get_language)
+})
+.bind(("127.0.0.1", 80))?
+.run()
+.await
+
+
+
 }
---
+++ src/presentation/language.rs
@@ -0,0 +1,24 @@
+use actix_web::{
+    get,
+    post,
+    put,
+    error::ResponseError,
+    web::Path,
+    web::Json,
+    web::Data, 
+    HttpResponse,
+    http::{header::ContentType, StatusCode}
+};
+
+use serde::{Serialize, Deserialize};
+use derive_more::{Display};
+
+#[derive(Serialize, Deserialize)]
+pub struct LanguageIdentifier {
+    programming_language_identifier: String,
+}
+
+#[get("/language/{programming_language_identifier}")]
+pub async fn language(language_identifier: Path<LanguageIdentifier>) -> Json<String> {
+    return Json(language_identifier.into_inner().programming_language_identifier);
+}
