d8159a3293a1d8fdb134624f908eefc4320c59ed
admirsaheta <81534875+admirsaheta@users.noreply.github.com>
2022-09-20 19:50:31 +0200

    iterator.rs

--- src/iterator/mod.rs
+++ src/iterator/mod.rs
@@ -1,2 +1,14 @@
 mod pointer;
 mod bearer;
+mod char_type;
+
+use crate::{iterator::pointer::Pointer, Error};
+
+pub use bearer::Bearer;
+pub use char_type::CharType;
+
+pub struct Iterator {
+    bearers: Vec<Bearer>,
+    errors: Vec<Error>
+}
+
